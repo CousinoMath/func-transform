b08071564b03bfee549243cf9c756881dba2cce3
Andrew Cousino <cousinomath@gmail.com>
2019-07-03 13:49:37 -0500

    Pulled sliders out into html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-const board = JXG.JSXGraph.initBoard('jxgbox', { boundingbox: [-4, 6, 6, -4], axis: true });
-const a = board.create('slider', [[-3, 4.5], [0, 4.5], [-5, 1, 5]], { name: 'a' });
-const b = board.create('slider', [[-3, 3.5], [0, 3.5], [-5, 1, 5]], { name: 'b' });
-const c = board.create('slider', [[-3, 2.5], [0, 2.5], [-5, 0, 5]], { name: 'c' });
-const d = board.create('slider', [[-3, 1.5], [0, 1.5], [-5, 0, 5]], { name: 'd' });
+const xMin = -5;
+const xMax = 5;
+const yMin = -5;
+const yMax = 5;
+const board = JXG.JSXGraph.initBoard('jxgbox',
+    { boundingbox: [xMin, yMax, xMax, yMin], axis: true });
+let aValue = 1.0;
+let bValue = 1.0;
+let cValue = 0.0;
+let dValue = 0.0;
+aSlider.addEventListener('input', (evt) => {
+    aValue = Number.parseFloat(aSlider.value);
+    aLabel.innerText = `a = ${aValue.toFixed(1)}`;
+    board.update();
+});
+bSlider.addEventListener('input', (evt) => {
+    bValue = Number.parseFloat(bSlider.value);
+    bLabel.innerText = `b = ${bValue.toFixed(1)}`;
+    board.update();
+});
+cSlider.addEventListener('input', (evt) => {
+    cValue = Number.parseFloat(cSlider.value);
+    cLabel.innerText = `c = ${cValue.toFixed(1)}`;
+    board.update();
+});
+dSlider.addEventListener('input', (evt) => {
+    dValue = Number.parseFloat(dSlider.value);
+    dLabel.innerText = `d = ${dValue.toFixed(1)}`;
+    board.update();
+});
 const base = (x) => 2 * x / (1 + x * x);
-const f = board.create('functiongraph', [(x) => base(x)], { strokeColor: 'black', dash: 1 });
-const tf = board.create('functiongraph', [(x) => a.Value() * base(b.Value() * x + c.Value()) + d.Value()]);
-const pt = board.create('glider', [0, 0, f], { fillColor: 'red', strokeColor: 'red', withLabel: false });
-const tpt = board.create('point', [() => (pt.X() - c.Value()) / b.Value(), () => a.Value() * pt.Y() + d.Value()], { fillColor: 'blue', strokeColor: 'blue', withLabel: false });
+const f = board.create('functiongraph',
+    [(x) => base(x), xMin, xMax],
+    { strokeColor: 'black', dash: 1 });
+const tf = board.create('functiongraph',
+    [(x) => aValue * base(bValue * x + cValue) + dValue, xMin, xMax]);
+const pt = board.create('glider', [0, 0, f],
+    { fillColor: 'red', strokeColor: 'red', withLabel: false });
+const tpt = board.create('point',
+    [() => (pt.X() - cValue) / bValue, () => aValue * pt.Y() + dValue],
+    { fillColor: 'blue', strokeColor: 'blue', withLabel: false });
